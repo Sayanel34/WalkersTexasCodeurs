fca2eeb5d18d08841efbed038adeb9943320a18e
Sayanel34 <steeve.beloncle@free.fr>
2015-03-24 11:30:20 +0100

    dll

---
+++ dllLastFm/lastFMApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dllLastFm.Metiers;
+
+namespace dllLastFm.Api
+{
+    class lastFMApi
+    {
+        private static string url_getEventInfosById = "ws.audioscrobbler.com/2.0/?method=event.getinfos&event=#idevent&api_key=fa23df2e235d7848831c147d76d76ba5";
+    }
+}
